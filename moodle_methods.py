e55735c1fffddd19ab9d05887b557c5d134f7080
Ema <101311799+Ema346@users.noreply.github.com>
2022-03-31 12:54:28 -0700

    Update moodle_methods.py

--- moodle_methods.py
+++ moodle_methods.py
@@ -6,10 +6,19 @@ from selenium.webdriver.common.by import By
 import moodle_locators as locators
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select  # <--- add this import for drop down lists
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
-s = Service(executable_path='../chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(options=options)
 
 
 def setUp():
@@ -259,3 +268,4 @@ def delete_user():
 # delete_user()
 # log_out()
 # tearDown()
+
